94ad5bc282c7b9d60efc58e715b75c74e9e0358c
Zhenёq <evgeniyspam@ya.ru>
2015-05-16 21:30:53 +0300

    Runge_Kutta for systems

--- main.py
+++ main.py
@@ -1,12 +1,42 @@
 import math
 from rk import *
 
+
 def f(t, y):
     return y
 
+
+def f_(t, y):
+    return y[0]
+
+
+def f0(t, yv):
+    return (2*t*(1 - t**2) + yv[1] * (1 + t**2)) / (1 - t - t**2)
+
+
+def f1(t, yv):
+    return (2 * t * yv[1] + f0(t, yv)) / (1 - t**2)
+
+
 h = 0.01
 y = 1.0
+#
+# for i in xrange(0, 100):
+#     x = i * h
+#     y = rungekutta(x, y, f, h)
+#     print y - math.exp(x + h)
+
+y = [1.0]
 for i in xrange(0, 100):
     x = i * h
-    y = rungekutta(x, y, f, h)
-    print y - math.exp(x + h)
+    y = rungekuttavec(x, y, [f_], h)
+    print y[0] - math.exp(x + h)
+
+# fv = [f0, f1]
+# yv = [0., 0.]
+# for i in xrange(0, 90):
+#     x = i * h
+#     y = rungekuttavec(x, yv, fv, h)
+#     print y[0] - (x**2 * (1 - x**2)) / (1 - x - x**2)
+#     print y[1] - x**2 / (1 - x - x**2)
+
--- rk.py
+++ rk.py
@@ -10,8 +10,40 @@ c = [0, 1/2., 1/2., 1]
 steps = len(b)
 
 
+def appendto(_list, _element):
+    _list.append(_element)
+    return _list
+
+
 def rungekutta(t, y, f, h):
     k = []
     for i in xrange(steps):
         k.append(h * f(t + c[i]*h, y + reduce(lambda x, y: x+y, map(lambda x, y: x*y, a[i], k), 0)))
     return y + reduce(lambda x, y: x+y, map(lambda x, y: x*y, b, k))
+
+
+def rungekuttavec(t, yv, fv, h):
+    kv = []
+    for i in xrange(steps):
+        # print "step = " + str(i)
+        # print kv
+        # print a[i]
+        ki = []
+        for idx in xrange(len(yv)):
+            # print "idx = " + str(idx)
+            tyv = map(lambda _factor, _vector: map(lambda _vector_elem: _factor * _vector_elem, _vector), a[i], kv)
+            tyv = reduce(lambda _vec1, _vec2: map(
+                lambda _vec1_elem, _vec2_elem: _vec1_elem + _vec2_elem,
+                _vec1, _vec2
+            ), tyv, [0. for q in xrange(len(yv))])
+            tyv = map(lambda _vec1_elem, _vec2_elem: _vec1_elem + _vec2_elem, tyv, yv)
+            ki.append(h * fv[idx](t + c[i]*h, tyv))
+        kv.append(ki)
+
+    res = map(lambda _factor, _vector: map(lambda _vector_elem: _factor * _vector_elem, _vector), b, kv)
+    res = reduce(lambda _vec1, _vec2: map(
+        lambda _vec1_elem, _vec2_elem: _vec1_elem + _vec2_elem,
+        _vec1, _vec2
+    ), res, [0. for q in xrange(len(yv))])
+    res = map(lambda _vec1, _vec2: _vec1 + _vec2, yv, res)
+    return res
